fe838b9fbe09aaa9426363d131393b35f88f460a
Philemon Forson <firstkoolaid21@gmail.com>
2024-02-08 20:24:43 +0000

    dockerized the project

--- src/test/java/com/project/transactions/TransactionsApplicationTests.java
+++ src/test/java/com/project/transactions/TransactionsApplicationTests.java
@@ -1,8 +1,12 @@
 package com.project.transactions;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class TransactionsApplicationTests {
 
